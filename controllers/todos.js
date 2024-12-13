554e416ab7bf42cbe95b043068936abcd2b83730
VI <ilmar.vodi@voco.ee>
2024-12-13 16:57:54 +0200

    patch working

--- controllers/todos.js
+++ controllers/todos.js
@@ -18,6 +18,31 @@ class todoController {
     getTodos(req, res) {
         res.json({tasks: this.TODOS})
     }
+
+    updateTodo(req, res) {
+        try {
+            const todoId = req.params.id
+            const updatedTask = req.body.task
+
+            const todoIndex = this.TODOS.findIndex((todo) => todo.id === todoId)
+
+            if(todoIndex < 0) {
+                res.json({
+                    message: 'could not find todos with such index'
+                })
+                throw new Error('could not find todo')
+            }
+
+            this.TODOS[todoIndex] = new Todo(this.TODOS[todoIndex].id, updatedTask)
+            res.json({
+                message: 'todo is updated',
+                updatedTask: this.TODOS[todoIndex]
+            })
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
 
 export const TodoController = new todoController()
